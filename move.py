7c23dd6e3e782b2bbcc68f5a52953626a04e8799
ashimajain2595 <ashimajain2595@gmail.com>
2016-04-22 06:51:15 +0530

    svm_dataset

---
+++ move.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 00:43:05 2016
+
+@author: rahul
+"""
+import os
+included_extensions = ['bmp']
+filelist = [fn[0:26] for fn in os.listdir('/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/Oxidation') if any([fn.endswith(ext) for ext in included_extensions])]
+filelist = list(set(filelist))
+print filelist
+oldpath = '/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/Oxidation/'
+newpath = '/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/svm_data_oxidation/'
+for fname in filelist:
+    oldname = oldpath + fname + 'c0.bmp'
+    newname = newpath + fname + 'c0.bmp'
+    os.rename(oldname, newname)
