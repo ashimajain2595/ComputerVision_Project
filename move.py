be96f6d894f0af42cde55787c5ea01d44a8eb435
ashimajain2595 <ashimajain2595@gmail.com>
2016-04-22 10:04:28 +0530

    svm_dataset

--- move.py
+++ move.py
@@ -6,12 +6,12 @@ Created on Sun Apr 17 00:43:05 2016
 """
 import os
 included_extensions = ['bmp']
-filelist = [fn[0:26] for fn in os.listdir('/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/Oxidation') if any([fn.endswith(ext) for ext in included_extensions])]
+filelist = [fn[0:26] for fn in os.listdir('/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/Oxidation/') if any([fn.endswith(ext) for ext in included_extensions])]
 filelist = list(set(filelist))
 print filelist
 oldpath = '/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/Oxidation/'
-newpath = '/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/svm_data_oxidation/'
+newpath = '/media/aj/OS/Users/SURENDRA JAIN/Documents/AJ/Acads/VIII sem/EE 625/ComputerVision_Project/Training images/Manual classification 02/svm_data_oxidation/c1/'
 for fname in filelist:
-    oldname = oldpath + fname + 'c0.bmp'
-    newname = newpath + fname + 'c0.bmp'
+    oldname = oldpath + fname + 'c1.bmp'
+    newname = newpath + fname + 'c1.bmp'
     os.rename(oldname, newname)
